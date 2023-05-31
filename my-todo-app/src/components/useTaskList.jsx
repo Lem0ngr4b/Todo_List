2654ebfbf44c8fd7f72b36d5a846139df290643b
cubo_de_hielo <cubodehielo385@gmail.com>
2023-05-31 15:22:07 -0500

    cambios4

---
+++ my-todo-app/src/components/useTaskList.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+
+export const useTaskList = () => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const storedTasks = localStorage.getItem('tasks');
+        if (storedTasks) {
+            setTasks(JSON.parse(storedTasks));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
+
+    const addTask = (newTask, newTaskDescription) => {
+        if (newTask.trim() !== '') {
+            const newTaskObject = {
+                id: Date.now(),
+                task: newTask,
+                description: newTaskDescription,
+                completed: false,
+            };
+            setTasks([...tasks, newTaskObject]);
+        }
+    };
+
+    const removeTask = (id) => {
+        const updatedTasks = tasks.filter((task) => task.id !== id);
+        setTasks(updatedTasks);
+    };
+
+    const updateTask = (id, newTask, newTaskDescription) => {
+        const updatedTasks = tasks.map((task) => {
+          if (task.id === id) {
+            return {
+              ...task,
+              task: newTask,
+              description: newTaskDescription,
+            };
+          }
+          return task;
+        });
+        setTasks(updatedTasks);
+      };
+      
+
+    return {
+        tasks,
+        addTask,
+        removeTask,
+        updateTask,
+    };
+}
+
+
