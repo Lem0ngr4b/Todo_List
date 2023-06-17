d4ecddfc0a40a4a0e971b3698f2c64f7a624b6e4
cubo_de_hielo <cubodehielo385@gmail.com>
2023-06-17 17:47:23 -0500

    chakra2

--- my-todo-app/src/App.jsx
+++ my-todo-app/src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { TodoList } from './components/TodoList'
+import { ChakraProvider } from '@chakra-ui/react';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
+    <ChakraProvider>
       <TodoList />
-    </div>
+    </ChakraProvider>
   );
 }
-
-export default App
